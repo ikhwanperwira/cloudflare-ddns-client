2d661962d5c9d5bed83d6bd09be5b8b0ec0fa683
wawan-ikhwan <ikhwanperwira@gmail.com>
2024-04-19 16:16:34 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Cloudflare DDNS Client
+
+## Introduction
+The `cf_ddns.py` script is a command-line tool that allows you to update your Cloudflare DNS records with your current IP address. This is useful for dynamic IP addresses where you want to keep your DNS records up to date.
+
+## Prerequisites
+Before using `cf_ddns.py`, make sure you have the following prerequisites installed:
+
+- Python 3.x
+- Cloudflare API Token with these permission:
+  ```
+  All zones - Zone Settings:Read, Zone:Read, DNS:Edit
+  ```
+
+## Installation
+Clone the repository:
+  ```shell
+  git clone https://github.com/ikhwanperwira/cloudflare-ddns-client.git
+  ```
+
+## Usage
+To use `cf_ddns.py`, follow these steps:
+
+1. Open a terminal or command prompt.
+
+2. Navigate to the project directory:
+    ```shell
+    cd ./cloudflare-ddns-client
+    ```
+
+3. Run the script with the following command:
+    ```shell
+    python3 cf_ddns.py python cf_ddns.py <API_TOKEN> <DOMAIN> <RECORD_NAME> <TTL> <PROXIED> <SOURCE_IP (OPTIONAL)>
+    ```
+
+    Example:
+    ```shell
+    python3 cf_ddns.py <API_TOKEN> example.com subdomain 3600 true 172.16.0.2
+    ```
+
+4. The script will retrieve your current public IP address and update the specified DNS record with the new IP address.
+
+## Startup Configuration
+
+To configure the `cf_ddns.py` script to run as a systemd service on Linux, follow these steps:
+
+1. Move `cf_ddns.py` to `/usr/bin`:
+  ```shell
+  sudo mv cf_ddns.py /usr/bin
+  ```
+
+2. Create a new service file using a text editor. For example, create a file called `cf_ddns.service` in the `/etc/systemd/system/` directory.
+    ```shell
+    sudo vi /etc/systemd/system/cf_ddns.service
+    ```
+
+3. Add the following content to the `cf_ddns.service` file:
+    ```
+    [Unit]
+    Description=Cloudflare DDNS Client
+    After=network.target
+
+    [Service]
+    Type=simple
+    ExecStart=/usr/bin/cf_ddns.py <API_TOKEN> <DOMAIN> <RECORD_NAME> <TTL> <PROXIED> <SOURCE_IP (OPTIONAL)>
+
+    [Install]
+    WantedBy=multi-user.target
+    ```
+
+4. Enable the service to start on boot and start the service:
+    ```shell
+    sudo systemctl enable cf_ddns.service
+    sudo systemctl start cf_ddns.service
+    ```
+
+## License
+This project is licensed under the [MIT License](LICENSE).
---
+++ cf_ddns.py
@@ -0,0 +1,271 @@
+#!/usr/bin/python3
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=line-too-long
+# pylint: disable=broad-exception-raised
+# pylint: disable=broad-exception-caught
+
+import sys
+import http.client
+import json
+import datetime
+from time import sleep
+
+if __name__ == '__main__':
+
+  # Read arguments
+  if len(sys.argv) < 6:
+    print('Usage: python cf_ddns.py <API_TOKEN> <DOMAIN> <RECORD_NAME> <TTL> <PROXIED> <SOURCE_IP (OPTIONAL)>')
+    sys.exit(1)
+
+  API_TOKEN = sys.argv[1]
+  DOMAIN = sys.argv[2]
+  RECORD_NAME = sys.argv[3]
+  TTL = int(sys.argv[4])
+  PROXIED = True if sys.argv[5] == 'true' else False
+  SOURCE_IP = sys.argv[6] if len(sys.argv) == 7 else None
+
+  API_BASE = 'api.cloudflare.com'
+
+  def get_current_public_ip(source_ip=None) -> str:
+    print()
+    print('Getting current public IP, using source IP:',
+          source_ip if source_ip is not None else 'None')
+
+    if source_ip is not None:
+      conn = http.client.HTTPConnection(
+          'ifconfig.me', source_address=(source_ip, 0))
+    else:
+      conn = http.client.HTTPConnection('ifconfig.me')
+    conn.request('GET', '/')
+    response = conn.getresponse()
+    data = response.read().decode()
+    conn.close()
+    if response.status == 200:
+      print('Current public IP:', data)
+      return data
+    else:
+      raise Exception('Failed to get public IP')
+
+  if SOURCE_IP is not None:
+    RECORD_VALUE = get_current_public_ip(SOURCE_IP)
+  else:
+    RECORD_VALUE = get_current_public_ip()
+
+  # Check IP version
+  if '.' in RECORD_VALUE:
+    RECORD_TYPE = 'A'
+  elif ':' in RECORD_VALUE:
+    RECORD_TYPE = 'AAAA'
+
+  def perform_http_request(api_token: str, method: str, endpoint: str, payload: dict = None) -> dict:
+
+    # Set the request headers
+    headers = {
+        'Authorization': f'Bearer {api_token}',
+        'Content-Type': 'application/json'
+    }
+
+    conn = http.client.HTTPSConnection(API_BASE)
+
+    # Send a GET request
+    if payload is None:
+      conn.request(method, endpoint, headers=headers)
+    else:
+      conn.request(method, endpoint, headers=headers, body=json.dumps(payload))
+
+    # Get the response
+    response = conn.getresponse()
+
+    status = response.status
+    data = json.loads(response.read().decode())
+
+    # Close the connection
+    conn.close()
+
+    # Print the response status code
+    print('Response Status:', status)
+
+    # Print the response body
+    print('Response Body:', data)
+
+    # Parsing JSON
+    return status, data
+
+  def verify_token(api_token: str) -> bool:
+    print()
+    print('Verifying API Token...')
+
+    status, _ = perform_http_request(
+        api_token, 'GET', '/client/v4/user/tokens/verify')
+
+    if status == 200:
+      print('Token is valid.')
+      return True
+
+    print('Token is invalid.')
+    return False
+
+  def get_zone_id(api_token: str, domain: str) -> str:
+    # Print a blank line for readability
+    print()
+
+    # Print a message indicating that we are listing zones
+    print('Listing Zones...')
+
+    # Perform an HTTP request to get the list of zones
+    status, data = perform_http_request(api_token, 'GET', '/client/v4/zones')
+
+    if status != 200:
+      # If the request was not successful, print an error message and return None
+      print('Failed to list zones.')
+      raise Exception('Failed to list zones.')
+
+    # Print a message indicating that we are getting the Zone ID of the specified domain
+    print('Getting Zone ID of domain:', domain)
+
+    # Iterate over the list of zones returned by the API
+    for zone in data['result']:
+      # Check if the name of the zone matches the specified domain
+      if zone['name'] == domain:
+        # If a match is found, retrieve the Zone ID and print it
+        zone_id = zone['id']
+        print(f'Zone ID of {domain}:', zone_id)
+        return zone_id
+
+    # If no match is found, print a message indicating that the Zone ID was not found
+    print(f'Zone ID of {domain} not found')
+    return None
+
+  def get_record_id(api_token: str, record_name: str, domain: str, zone_id: str) -> str:
+    # Print a blank line for readability
+    print()
+
+    # Print a message indicating that we are getting DNS records
+    print('Getting DNS records...')
+
+    # Perform an HTTP request to get the DNS records for the specified zone
+    status, data = perform_http_request(
+        api_token, 'GET', f'/client/v4/zones/{zone_id}/dns_records')
+
+    # Check if the request was successful
+    if status != 200:
+      # If the request was not successful, print an error message and return None
+      print('Failed to get DNS records.')
+      raise Exception('Failed to get DNS records')
+
+    # Print a message indicating that we are finding the DNS record ID
+    print(f'Finding DNS record ID of {record_name}.{domain}...')
+
+    # Iterate over the DNS records returned by the API
+    for record in data['result']:
+      # Check if the name of the record matches the specified record name and domain
+      if record['name'] == f'{record_name}.{domain}':
+        # If a match is found, retrieve the record ID and print it
+        record_id = record['id']
+        print(f'DNS Record ID of {record_name}.{domain}:', record_id)
+        return record_id
+
+    # If no match is found, print a message indicating that the DNS record was not found
+    print(f'DNS Record {record_name}.{domain} not found')
+    return None
+
+  def overwrite_record(api_token: str, record_type: str, record_name: str, domain: str, record_value: str, ttl: int, proxied: bool, zone_id: str, record_id: str) -> bool:
+    # Print a blank line for readability
+    print()
+    # Print a message indicating that we are overwriting the DNS record
+    print('Overwriting DNS record...')
+
+    # Get the current timestamp
+    timestamp = datetime.datetime.now().isoformat()
+
+    # Create the payload for the API request
+    payload = {
+        'content': record_value,
+        'name': f'{record_name}.{domain}',
+        'proxied': proxied,
+        'type': record_type,
+        'comment': f'Updated by DDNS Client at {timestamp}.',
+        'ttl': ttl
+    }
+
+    # Perform the HTTP request to update the DNS record
+    status, _ = perform_http_request(
+        api_token, 'PUT', f'/client/v4/zones/{zone_id}/dns_records/{record_id}', payload)
+
+    # Check if the request was successful
+    if status != 200:
+      # If the request was not successful, print an error message and return None
+      print('Failed to update DNS record.')
+      return False
+
+    # Print a message indicating that the DNS record was successfully updated
+    print(f'{RECORD_NAME}.{DOMAIN} with type {RECORD_TYPE} updated to {RECORD_VALUE} where TTL is {TTL} and Proxied is {PROXIED}.')
+    return True
+
+  def add_record(api_token: str, record_type: str, record_name: str, domain: str, record_value: str, ttl: int, proxied: bool, zone_id: str) -> bool:
+    # Print a blank line for readability
+    print()
+    # Print a message indicating that we are overwriting the DNS record
+    print('Adding DNS record...')
+
+    # Get the current timestamp
+    timestamp = datetime.datetime.now().isoformat()
+
+    # Create the payload for the API request
+    payload = {
+        'content': record_value,
+        'name': f'{record_name}.{domain}',
+        'proxied': proxied,
+        'type': record_type,
+        'comment': f'Created by DDNS Client at {timestamp}.',
+        'ttl': ttl
+    }
+
+    # Perform the HTTP request to update the DNS record
+    status, _ = perform_http_request(
+        api_token, 'POST', f'/client/v4/zones/{zone_id}/dns_records', payload)
+
+    # Check if the request was successful
+    if status != 200:
+      # If the request was not successful, print an error message and return None
+      print('Failed to create DNS record.')
+      return False
+
+    # Print a message indicating that the DNS record was successfully updated
+    print(f'{record_name}.{domain} with type {record_type} created to {record_value} where TTL is {ttl} and Proxied is {proxied}.')
+    return True
+
+  def perform_ddns(api_token: str, domain: str, record_type: str, record_name: str, record_value: str, ttl: int, proxied: bool) -> bool:
+    # Verify the token
+    if not verify_token(api_token):
+      return False
+
+    # Get the zone ID
+    zone_id = get_zone_id(api_token, domain)
+    if zone_id is None:
+      return False
+
+    # Get the record ID
+    record_id = get_record_id(api_token, record_name, domain, zone_id)
+
+    # If record ID is not found, add a new record
+    if record_id is None:
+      return add_record(api_token, record_type, record_name, domain, record_value, ttl, proxied, zone_id)
+
+    # Overwrite the existing record
+    return overwrite_record(api_token, record_type, record_name, domain, record_value, ttl, proxied, zone_id, record_id)
+
+  MAX_RETRY = 3
+
+  while MAX_RETRY > 0:
+    try:
+      perform_ddns(API_TOKEN, DOMAIN, RECORD_TYPE,
+                   RECORD_NAME, RECORD_VALUE, TTL, PROXIED)
+      break
+    except Exception as e:
+      print('An error occurred:', e)
+      MAX_RETRY -= 1
+      sleep(5)
+      continue
